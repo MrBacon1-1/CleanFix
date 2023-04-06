ef687064d914cd71782fb55703abbd32591bd69e
MrBacon <105984061+MrBacon1-1@users.noreply.github.com>
2023-04-06 19:09:20 +0100

    Add files via upload

---
+++ System Repair.bat
@@ -0,0 +1,122 @@
+@echo off
+
+:main
+
+color 2
+title System Repair By MrBacon
+cls
+
+mode con: cols=80 lines=30
+
+echo ~Multi-Tool By MrBacon#7458~
+echo ----------------
+echo [1] Currupted File Check / System Repair
+echo [2] Fix IP / Reset IP
+echo [3] Clean Temp Files
+echo ----------------
+
+set /p opt= Option: 
+
+if %opt% == 1 goto cfile
+if %opt% == 2 goto ipfix
+if %opt% == 3 goto temp
+if %opt% != 1 or 2 or 3 exit
+
+REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+REM System Fixing Commands
+REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+:cfile
+cls
+
+echo [?] Press Enter To Begin (Restart At The End)
+pause >null
+cls
+
+echo [!] Checking For Curupted Files
+dism.exe /online /cleanup-image /restorehealth
+echo [?] Step One Done
+timeout 5
+
+cls
+
+echo [!] Checking For Curupted Files
+dism.exe /online /cleanup-image /scanhealth
+echo [?] Step Two Done
+timeout 5
+
+cls
+
+echo [!] Checking For Curupted Files
+SFC /scannow
+
+cls
+
+echo [?] Step Three Done Restarting In 5 Seconds
+timeout 5 >null
+shutdown.exe /r /t 00
+
+REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+REM IP Fixing Commands
+REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+:ipfix
+cls
+
+echo [?] Press Enter To Begin (Restart At The End)
+pause >null
+cls
+
+echo [?] Fixing IP 1/5
+netsh winsock reset
+timeout 1 >null
+
+cls
+
+echo [?] Fixing IP 2/5
+netsh int ip reset
+timeout 1 >null
+
+cls
+
+echo [?] Fixing IP 3/5
+ipconfig /release
+timeout 1 >null
+
+cls
+
+echo [?] Fixing IP 4/5
+ipconfig /renew
+timeout 1 >null
+
+cls
+
+echo [?] Fixing IP 5/5
+ipconfig /flushdns
+timeout 1 >null
+
+cls 
+
+echo [?] Done, Restarting In 5 Seconds
+timeout 5 >null
+shutdown.exe /r /t 00
+
+REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+REM Clean Temp Files
+REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+:temp
+cls
+
+echo [?] Cleaning temporary files...
+
+del /q /s "%TEMP%\*.*"
+
+cls
+echo [?] Temporary files have been cleaned.
+timeout 2 >null
+
+goto main
+
+
+
