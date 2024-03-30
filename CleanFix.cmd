177c4f4c82e13622ad6cc27356d0b5e67d669d52
MrBacon <fabianmachowski@gmail.com>
2024-03-30 15:42:24 +0000

    Better Temp Cleaner

--- CleanFix.cmd
+++ CleanFix.cmd
@@ -2,7 +2,7 @@
 
 if not "%1"=="am_admin" (
     echo Checking For Admin Permissions...
-    timeout 1 >null
+    timeout 1 >nul
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
     exit /b
 )
@@ -50,28 +50,28 @@ REM ----------------------------------------------------------------------------
 cls
 
 echo [CleanFix] Press Enter To Begin... (Restart At The End)
-pause >null
+pause >nul
 cls
 
-echo [CleanFix] Checking For Curupted Files (1/3)
-dism.exe /online /cleanup-image /restorehealth
-timeout 5
-
-cls
-
-echo [CleanFix] Checking For Curupted Files (2/3)
-dism.exe /online /cleanup-image /scanhealth
-timeout 5
+echo [CleanFix] Checking For Corrupted Files (1/3)
+dism.exe /online /cleanup-image /restorehealth >nul 2>&1
+timeout 2 >nul
 
 cls
 
-echo [CleanFix] Checking For Curupted Files (3/3)
-SFC /scannow
+echo [CleanFix] Checking For Corrupted Files (2/3)
+dism.exe /online /cleanup-image /scanhealth >nul 2>&1
+timeout 2 >nul
+
+cls
+
+echo [CleanFix] Checking For Corrupted Files (3/3)
+SFC /scannow >nul 2>&1
 
 cls
 
 echo [CleanFix] Restarting In 5 Seconds...
-timeout 5 >null
+timeout 5 >nul
 shutdown.exe /r /t 00
 
 REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -82,41 +82,41 @@ REM ----------------------------------------------------------------------------
 cls
 
 echo [CleanFix] Press Enter To Begin... (Restart At The End)
-pause >null
+pause >nul
 cls
 
 echo [CleanFix] Fixing IP 1/5
-netsh winsock reset >nul
-timeout 3 >null
+netsh winsock reset >nul 2>&1
+timeout 3 >nul
 
 cls
 
 echo [CleanFix] Fixing IP 2/5
-netsh int ip reset >nul
-timeout 3 >null
+netsh int ip reset >nul 2>&1
+timeout 3 >nul
 
 cls
 
 echo [CleanFix] Fixing IP 3/5
-ipconfig /release >nul
-timeout 3 >null
+ipconfig /release >nul 2>&1
+timeout 3 >nul
 
 cls
 
 echo [CleanFix] Fixing IP 4/5
-ipconfig /renew >nul
-timeout 3 >null
+ipconfig /renew >nul 2>&1
+timeout 3 >nul
 
 cls
 
 echo [CleanFix] Fixing IP 5/5
-ipconfig /flushdns >nul
-timeout 3 >null
+ipconfig /flushdns >nul 2>&1
+timeout 3 >nul
 
 cls 
 
 echo [CleanFix] Restarting In 5 Seconds
-timeout 5 >null
+timeout 5 >nul
 shutdown.exe /r /t 00
 
 REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -127,27 +127,51 @@ REM ----------------------------------------------------------------------------
 cls
 
 echo [CleanFix] Press Enter To Begin...
-pause >null
+pause >nul
 
-echo [CleanFix] Cleaning temporary files...
+echo [CleanFix] Clearing CryptNet SSL Certificate Cache...
 
-rd /s /q "%temp%"
+certutil -URLcache * delete >nul 2>&1
 
-FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
-for /F "tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G")
-echo.
-goto theEnd
+echo [CleanFix] Done Clearing CryptNet SSL Certificate Cache.
 
-:do_clear
-echo clearing %1
-wevtutil.exe cl %1
-goto :eof
+echo [CleanFix] Clearing Windows Update Cache...
 
+net stop WUAUSERV >nul 2>&1
+if exist %windir%\softwaredistribution\download rmdir /s /q %windir%\softwaredistribution\download >nul 2>&1
+net start WUAUSERV >nul 2>&1
 
-:theEnd
-cls
-echo [CleanFix] Temporary files have been cleaned.
-timeout 2 >null
+echo [CleanFix] Done Clearing Windows Update Cache.
+
+echo [CleanFix] Clearing System Temp Files...
+
+del /F /S /Q "%WINDIR%\TEMP\*" >nul 2>&1
+
+if exist "%ProgramFiles%\Nvidia Corporation\Installer2" rmdir /s /q "%ProgramFiles%\Nvidia Corporation\Installer2" >nul 2>&1
+if exist "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService" del /f /q "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService\*.exe" >nul 2>&1
+
+if exist %SystemDrive%\MSOCache rmdir /S /Q %SystemDrive%\MSOCache >nul 2>&1
+
+if exist %SystemDrive%\i386 rmdir /S /Q %SystemDrive%\i386 >nul 2>&1
+
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick" >nul 2>&1
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource" >nul 2>&1
+
+if exist "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp" >nul 2>&1
+
+if exist "%ProgramFiles%\NVIDIA Corporation\Installer" rmdir /s /q "%ProgramFiles%\NVIDIA Corporation\Installer" >nul 2>&1
+if exist "%ProgramFiles%\NVIDIA Corporation\Installer2" rmdir /s /q "%ProgramFiles%\NVIDIA Corporation\Installer2" >nul 2>&1
+if exist "%ProgramFiles(x86)%\NVIDIA Corporation\Installer" rmdir /s /q "%ProgramFiles(x86)%\NVIDIA Corporation\Installer" >nul 2>&1
+if exist "%ProgramFiles(x86)%\NVIDIA Corporation\Installer2" rmdir /s /q "%ProgramFiles(x86)%\NVIDIA Corporation\Installer2" >nul 2>&1
+if exist "%ProgramData%\NVIDIA Corporation\Downloader" rmdir /s /q "%ProgramData%\NVIDIA Corporation\Downloader" >nul 2>&1
+if exist "%ProgramData%\NVIDIA\Downloader" rmdir /s /q "%ProgramData%\NVIDIA\Downloader" >nul 2>&1
+
+echo [CleanFix] Done Clearing System Temp Files.
+
+echo [CleanFix] Press Enter To Return To Menu...
+pause >nul
+
+goto main
 
 REM -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 REM Extras
